0e1214f55e9004575e9079a931531e0e4063ed59
ekronke <ekronke@kth.se>
2024-06-22 22:59:35 +0200

    No clue

--- deck.cpp
+++ deck.cpp
@@ -1,39 +1,13 @@
-#include <stack>
+#include <cstdio>
+#include <numeric>
+#include <utility>
 #include <vector>
+#include <iostream>
 
-// CHANGE stack to vector
-
-// Switch case for numbers to correct card
-
-// Create a deck
-std::stack<int> createDeck() 
-{	
-	std::stack<int> deck;
-	for (int i = 1; i <= 52; i++)
-	{
-		deck.push(i);
-	}
-	return deck;
-}
-
-// Draw
-std::pair<std::stack<int>,std::vector<int>> drawCards(std::stack<int> deck, int n)
-{
-	for (int i = 1; i < n; i++) 
-	{
-		vector<int> drawnCards
-	
-	}
-	return std:make_pair(deck, drawnCards)
-}
-
-
-// Shuffle
-
-
-// Deal
 
 int main() {
+	std::vector<int> deckOfCards = createDeck();
+	int firstCard = deckOfCards[0];
 
-
+	std::cout << firstCard; 
 }
